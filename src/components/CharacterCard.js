26d0f046fb058c17ae87d339f9ec8ae966361acd
Hector Valdez <hvaldez180@gmail.com>
2019-08-23 22:23:08 -0400

    made species and status on same line

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -5,7 +5,6 @@ export default function CharacterCard(props) {
   return <div key={props.id}>
     <img src={props.image}/>
     <h4>{props.name}</h4>
-    <p>{props.species}</p>
-    <p>{props.status}</p>
+    <p>{props.species} {props.status} </p>
   </div>
 }
