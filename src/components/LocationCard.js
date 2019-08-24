9ad3ad289bae165ed8610ac2be71be5be4c480d2
Hector Valdez <hvaldez180@gmail.com>
2019-08-23 23:21:52 -0400

    added loction card

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { Card } from "semantic-ui-react"; 
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
+export default function LocationCard(props) {
+  return (
+     <Card>
+    <Card.Content>
+      <Card.Header>{props.name}</Card.Header>
+      <Card.Meta>{props.type} {props.dimension}</Card.Meta>
+      </Card.Content>
+    </Card>
+  )
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
+import LocationCard from './LocationCard';
 import axios from 'axios';
 
-
 export default function LocationsList() {
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
   useEffect(() => {
     axios.get(`https://rickandmortyapi.com/api/location/`)
     .then(response => {
@@ -14,7 +14,10 @@ export default function LocationsList() {
   }, []);
   return (
     <section className="location-list grid-view">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {data.map( res => (
+        <LocationCard key={res.id} name={res.name} type={res.type} dimension={res.dimension}/>
+      
+      ))}
     </section>
   );
 }
