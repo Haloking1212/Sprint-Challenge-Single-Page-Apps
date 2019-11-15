ec8f4141ee9ccb81722d6319333ddc619edd835c
Hector Valdez <hvaldez180@gmail.com>
2019-11-15 16:16:25 -0500

    made a nav bar and seperated components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { Route } from "react-router-dom";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList.js";
+import SearchForm from "./components/SearchForm";
 
 
 export default function App() {
   return (
     <main>
       <Header />
-      {/* <TabNav /> */}
-      <Route path="/" component={CharacterList}  />
+      <TabNav />
+      <Route path="/characters" render={ () => <CharacterList/>}  />
+      <Route path="/search" render={ () => <SearchForm/>}  />
     </main>
   );
 }
--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,11 +1,45 @@
 import React from "react";
-import { Tab, Menu, Icon } from "semantic-ui-react";
-import { NavLink } from "react-router-dom";
+// import { Tab, Menu, Icon } from "semantic-ui-react";
+// import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import styled from 'styled-components'
+
+const Div = styled.div`
+display: flex;
+justify-content: space-between;
+text-align: center;
+`
+
 
 // TODO: Add missing tabs below
 // Take a look at React Semantic UI tabs 
 // https://react.semantic-ui.com/modules/tab/
 export default function TabNav() {
 
-};
+    return (
+        <Div className="nav-bar">
+            <div>
+            <Link to="/">Home</Link>
+            </div>
 
+            <div>
+            <Link to="/characters">Characters</Link>
+            </div>
+
+            <div>
+            <Link to="/episodes">Episodes</Link>
+            </div>
+
+            <div>
+            <Link to="/locations">Location</Link>
+            </div>
+
+            <div>
+            <Link to="/search">Search</Link>
+            </div>
+
+        </Div>
+    )
+    
+
+};
