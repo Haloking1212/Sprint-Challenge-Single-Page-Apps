80b8f8bc38bc22bc151cba5efd8e2859a391df21
Hector Valdez <hvaldez180@gmail.com>
2019-11-15 13:39:08 -0500

    added character list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
+import CharacterList from "./components/CharacterList.js";
 
 
 export default function App() {
@@ -8,6 +10,7 @@ export default function App() {
     <main>
       <Header />
       {/* <TabNav /> */}
+      <Route path="/" component={CharacterList}  />
     </main>
   );
 }
